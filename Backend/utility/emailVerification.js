40a7c8850be86f7a605a4fed1a5ceef9d26e1a49
Faisal Raza <faisalrazaqazi2000@gmail.com>
2025-09-06 15:25:59 +0500

    some change

--- Backend/utility/emailVerification.js
+++ Backend/utility/emailVerification.js
@@ -39,7 +39,7 @@ const sendEmail = async (email) => {
 const pas_Email = async (email, token) => {
     try {
         const transactionalEmailsApi = new SibApiV3Sdk.TransactionalEmailsApi();
-        const link = `${process.env.BASE_URL}/change-password/${token}`;
+        const link = `http://localhost:5173/change-password/${token}`;
 
         const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
         sendSmtpEmail.to = [{ email }];
